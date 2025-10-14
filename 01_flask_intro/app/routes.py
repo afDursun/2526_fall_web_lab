d507ad965c6d6ed6c2294959452c8e456398ac92
Ahmet Faruk Dursun <faruk.dursun@bil.omu.edu.tr>
2025-10-14 09:55:01 +0300

    init: initial Flask project setup

---
+++ 01_flask_intro/app/routes.py
@@ -0,0 +1,18 @@
+from flask import render_template
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Kagan'}
+    posts = [
+        {
+            'author': {'username': 'Ahmet'},
+            'body': 'Beautiful day in Samsun!'
+        },
+        {
+            'author': {'username': 'Hami'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
