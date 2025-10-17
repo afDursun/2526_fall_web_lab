ea3c20016dc6b0252e3528ca30f223a9f56f8216
Ahmet Faruk Dursun <faruk.dursun@bil.omu.edu.tr>
2025-10-17 09:15:56 +0300

    feat: add about page (template inheritance)

--- 01_flask_intro/app/routes.py
+++ 01_flask_intro/app/routes.py
@@ -16,3 +16,8 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
